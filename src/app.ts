f79d8f9519cc58326049993ea7f93e309493a96d
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-08-06 20:20:03 -0300

    feat: add services on app

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,41 @@
  * @version 1.0.0
  */
 
-/** 
- * Primeiro preciso saber qual o sistema operacional que estou rodando,
- * e depois disso, preciso direcionar para o caminho correto que desejo acessar.
- * Obtendo o caminho correto, preciso informar se o caminho e no formato windows ou linux.
- * Se for windows, preciso usar a barra invertida, se for linux, preciso usar a barra normal.
- * Se for macOS, preciso usar a barra normal também.
- * Acessando o caminho correto, preciso verificar se o arquivo existe.
+import { OsType } from './model/os-type';
+import { PathFile } from './model/path-file';
+import { CheckPlatformService } from './services/check-platform-service';
+import { PathFileService } from './services/path-file-service';
+
+/**
  * Se o arquivo existir, preciso ler o arquivo e verificar a ultima modificação.
  * Se a ultima modificação for menor que 3 meses, compactar os arquivo.
  * Se a ultima modificação for maior que 3 meses, não compactar os arquivos.
  */
-import { ZipFileService } from './services/zip-files-service';
+
+async function main(): Promise<void> {
+	const osType = new OsType();
+	let pathFileService: PathFileService;
+	const osHomedir = osType.getOsHomedir();
+
+	const list = osType
+		.getPathfile()
+		.add(new PathFile('', osHomedir, 'Downloads'));
+
+	for (const path of list) {
+		try {
+			const way = path.getPath();
+
+			pathFileService = new PathFileService(new CheckPlatformService());
+			const join = pathFileService.safeJoin(way, pathFileService.removePaths);
+
+			if (await pathFileService.isDirectoryExists(join)) {
+				path.addPath(join);
+			}
+			console.log(path.getPath());
+		} catch (err) {
+			console.error(err);
+		}
+	}
+}
+
+main();
--- src/services/path-file-service.ts
+++ src/services/path-file-service.ts
@@ -1,17 +1,21 @@
 import { access, constants } from 'node:fs/promises';
 import { isAbsolute, join, sep, win32 } from 'node:path';
+import { ErrorStatus } from '../error/enums/error-status';
+import { IOError } from '../error/io-error';
 import type { IOsType } from '../interface/i-os-type';
 
 export class PathFileService {
 	private osType: IOsType;
-
 	public constructor(osType: IOsType) {
 		this.osType = osType;
 	}
 
 	public removePaths(arr?: Array<string>): void {
 		if (arr?.length === 0 || arr?.length === undefined) {
-			throw new Error('Array be empty');
+			throw new IOError({
+				name: ErrorStatus.DIRECTORY_EMPTY, 
+				message: 'Diretory is not send'
+			});
 		}
 
 		for (let i: number = 0; i <= arr?.length; i++) {
@@ -39,13 +43,24 @@ export class PathFileService {
 		paths: Array<string>,
 		cb?: (arr?: Array<string>) => void
 	): string {
+		
+		if(paths.length === 0){
+			throw new IOError({
+				name: ErrorStatus.DIRECTORY_NOT_FOUND,
+				message: 'Directory not found'
+			})
+		}
+
 		const isWindows: string =
 			this.osType.getOsPlatform() === 'win32' ? win32.sep : sep;
 		const joinPath: string = join(...paths);
 
 		const pathAbsolute: string = joinPath.split(isWindows).join(isWindows);
 		if (pathAbsolute === '') {
-			throw new TypeError('join path must not be empty');
+			throw new IOError({
+				name: ErrorStatus.DIRECTORY_NOT_ABSOLUTE,
+				message: 'Directory not absolute'
+			});
 		}
 
 		if (cb) {
