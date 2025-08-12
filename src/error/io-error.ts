9b5254936b8bb8545b318f0d8474d795e37d3eea
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-08-11 21:44:34 -0300

    chore: adjustment on styles text

--- src/error/io-error.ts
+++ src/error/io-error.ts
@@ -1,4 +1,4 @@
-import { ErrorStatus } from "./enums/error-status";
+import type { ErrorStatus } from './enums/error-status';
 import { ErrorBase } from './utils/error-base';
 
 export class IOError extends ErrorBase<ErrorStatus> {}
