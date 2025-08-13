3411c3db7bfbf60bb418d166d66b346f621b1311
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-08-12 22:07:58 -0300

    refactor: removing pathIndex argument

--- src/interface/i-file-service.ts
+++ src/interface/i-file-service.ts
@@ -2,9 +2,5 @@ import type { WriteStream } from 'node:fs';
 
 export interface IFileService {
 	readFilesFromDirectory(dir: string): Promise<string[]>;
-	createWriteStreamForFile(
-		dir: string,
-		fileName: string,
-		pathIndex?: number
-	): Promise<WriteStream>;
+	createWriteStreamForFile(dir: string, fileName: string): Promise<WriteStream>;
 }
