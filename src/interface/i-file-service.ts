601f84f98b91c9f7991a7d5537c6b2f08f5b9c66
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-08-12 21:47:10 -0300

    refactor: remove argument index of the method readFilesFromDirectory

--- src/interface/i-file-service.ts
+++ src/interface/i-file-service.ts
@@ -2,5 +2,9 @@ import type { WriteStream } from 'node:fs';
 
 export interface IFileService {
 	readFilesFromDirectory(dir: string): Promise<string[]>;
-	createWriteStreamForFile(dir: string, fileName: string): Promise<WriteStream>;
+	createWriteStreamForFile(
+		dir: string,
+		fileName: string,
+		pathIndex?: number
+	): Promise<WriteStream>;
 }
