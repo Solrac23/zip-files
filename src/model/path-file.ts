3e5ccb2d263c4cd9782069e573c5528c925b2c8f
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-08-15 23:11:08 -0300

    refactor: adding model create, modifiedand sizeFile

--- src/model/path-file.ts
+++ src/model/path-file.ts
@@ -4,10 +4,16 @@ import { IOError } from '../error/io-error';
 export class PathFile {
 	private paths: Array<string>;
 	private files: Array<string>;
+	private sizeFile: number;
+	private create: Date;
+	private modified: Date;
 
 	public constructor(...initilaPaths: Array<string>) {
 		this.paths = initilaPaths || Array<string>;
 		this.files = [] as string[];
+		this.sizeFile = 0;
+		this.create = new Date();
+		this.modified = new Date();
 	}
 
 	public getPaths(): ReadonlyArray<string> {
@@ -30,5 +36,29 @@ export class PathFile {
 
 	public addFiles(...newFiles: string[]): void {
 		this.files.push(...newFiles);
+	}
+
+	public getSizeFile(): Readonly<number> {
+		return this.sizeFile;
+	}
+
+	public setSizeFile(sizeFile: number): void {
+		this.sizeFile = sizeFile;
+	}
+
+	public getCreate(): Readonly<Date> {
+		return this.create;
+	}
+
+	public setCreate(create: Date) {
+		this.create = create;
+	}
+
+	public getModified(): Readonly<Date> {
+		return this.modified;
+	}
+
+	public setModified(modified: Date) {
+		this.modified = modified;
 	}
 }
