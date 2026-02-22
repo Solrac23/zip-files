be4eb6b57acc7c7671bc584a53084b5d95570a17
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-02-22 13:41:36 -0300

    refactor: add constructor to initialize object platform

--- src/services/check-platform-service.ts
+++ src/services/check-platform-service.ts
@@ -1,7 +1,12 @@
+import { platform } from 'node:os';
 import type { IOsType } from '../interface/i-os-type';
 
 export class CheckPlatformService implements IOsType {
-	private platform!: NodeJS.Platform;
+	private platform: NodeJS.Platform;
+
+	public constructor() {
+		this.platform = platform();
+	}
 
 	public getOsPlatform(): NodeJS.Platform {
 		return this.platform;
