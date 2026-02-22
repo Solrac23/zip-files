18548ccc2abdcd5a0fd3562e06ce4e72c011d8b0
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-02-22 13:34:28 -0300

    refactor: removing object deprecated and unused

--- src/model/path-file.ts
+++ src/model/path-file.ts
@@ -2,32 +2,22 @@ import { ErrorStatus } from '../error/enums/error-status';
 import { IOError } from '../error/io-error';
 
 export class PathFile {
-	private paths: Array<string>;
+	private basePath: string;
 	private files: Array<string>;
-	private sizeFile: number;
-	private create: Date;
-	private modified: Date;
 
-	public constructor(...initilaPaths: Array<string>) {
-		this.paths = initilaPaths || Array<string>;
-		this.files = [] as string[];
-		this.sizeFile = 0;
-		this.create = new Date();
-		this.modified = new Date();
-	}
-
-	public getPaths(): ReadonlyArray<string> {
-		return this.paths;
-	}
-
-	public addPaths(path: string): void {
-		if (!path) {
+	public constructor(basePath: string) {
+		if (!basePath) {
 			throw new IOError({
 				name: ErrorStatus.INVALID_DIRECTORY,
-				message: "Path can't be empty",
+				message: "basePath can't be empty",
 			});
 		}
-		this.paths.push(path);
+		this.basePath = basePath;
+		this.files = [] as string[];
+	}
+
+	public getBasePath(): Readonly<string> {
+		return this.basePath;
 	}
 
 	public getFiles(): ReadonlyArray<string> {
@@ -36,29 +26,5 @@ export class PathFile {
 
 	public addFiles(...newFiles: string[]): void {
 		this.files.push(...newFiles);
-	}
-
-	public getSizeFile(): Readonly<number> {
-		return this.sizeFile;
-	}
-
-	public setSizeFile(sizeFile: number): void {
-		this.sizeFile = sizeFile;
-	}
-
-	public getCreate(): Readonly<Date> {
-		return this.create;
-	}
-
-	public setCreate(create: Date) {
-		this.create = create;
-	}
-
-	public getModified(): Readonly<Date> {
-		return this.modified;
-	}
-
-	public setModified(modified: Date) {
-		this.modified = modified;
 	}
 }
