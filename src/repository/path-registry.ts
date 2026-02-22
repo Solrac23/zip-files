e921a5999721dffae5d469214e2983abea97cf5a
Carlos Eduardo <eduardosantoos573@gmail.com>
2026-02-22 13:31:07 -0300

    refactor: create repository to storage files

---
+++ src/repository/path-registry.ts
@@ -0,0 +1,25 @@
+import { ErrorStatus } from '../error/enums/error-status';
+import { IOError } from '../error/io-error';
+import type { PathFile } from '../model/path-file';
+
+export class PathRegistry {
+	private pathFiles: Set<PathFile> = new Set();
+
+	public getPathFiles(): ReadonlySet<PathFile> {
+		return this.pathFiles;
+	}
+
+	public addPathFile(pathFile: PathFile): void {
+		if (!pathFile) {
+			throw new IOError({
+				name: ErrorStatus.INVALID_PATHFILE,
+				message: 'PathFile cannot be null or undefined',
+			});
+		}
+		this.pathFiles.add(pathFile);
+	}
+
+	public clear(): void {
+		this.pathFiles.clear();
+	}
+}
